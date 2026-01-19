e8141f7afe13a08cd9d17f1ebd0ee700b1b6eb0d
diegoAcuna1612 <u20201c794@upc.edu.pe>
2026-01-19 14:36:03 -0500

    feat: add middleware to all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
 import { ClientsModule } from './clients/clients.module';
 import { ServicesModule } from './services/services.module';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
-
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 @Module({
   imports: [CommonModule, UsersModule, ClientsModule, ServicesModule, SubscriptionsModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+   configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
